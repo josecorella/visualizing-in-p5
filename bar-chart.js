f08f2f5a34a79ec933102b32a7312e9931c112d8
Jose <joseeduardocorella@gmail.com>
2021-02-04 19:37:38 -0800

    I can now produce axes

--- bar-chart.js
+++ bar-chart.js
@@ -8,12 +8,21 @@ function preload() {
     'header');
 }
 
-let detailY;
 function setup() {
-  createCanvas(500, 500, WEBGL);
-  
+  createCanvas(1000, 500);
+  background(255);
+
   // how many rows?
   console.log(data.getRowCount());
   // what are the columns?
   console.log(data.columns);
+
+  var xLen = 700;
+  //draw xaxis
+  line(250, 400, 700, 400);
+
+  //draw yaxis
+  line(250, 400, 250, 50);
+
+
 }

b135b6c42ce16d8d4c59b1a86e49d3959200708d
Jose <joseeduardocorella@gmail.com>
2021-02-05 16:24:28 -0800

    more updates

--- bar-chart.js
+++ bar-chart.js
@@ -12,12 +12,23 @@ function setup() {
   createCanvas(1000, 500);
   background(255);
 
+  var all_titles = data.getColumn("Title"); //data for the y axis
+  var sales = {}
+  var titles = {}
+
+  for (var i = 0; i < 10; i++) {
+    titles[i] = all_titles[i];
+    sales[i] = data.getRow(i).getString("Metrics.Sales");
+  }
+
+  console.log(titles);
+  console.log(sales);
+
   // how many rows?
   console.log(data.getRowCount());
   // what are the columns?
   console.log(data.columns);
 
-  var xLen = 700;
   //draw xaxis
   line(250, 400, 700, 400);
 
